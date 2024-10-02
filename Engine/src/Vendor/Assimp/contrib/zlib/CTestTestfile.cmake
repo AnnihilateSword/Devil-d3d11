17c3a309f610c0cb17aefe7fa0184646fa009b17
jinkunou <743144825@qq.com>
2024-10-02 18:52:59 +0800

    [Fix]: fix .gitignore file, recover assimp cmake-modeles files.

---
+++ Engine/src/Vendor/Assimp/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/Dev/Devil/Engine/src/Vendor/Assimp/contrib/zlib
+# Build directory: D:/Dev/Devil/Engine/src/Vendor/Assimp/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
